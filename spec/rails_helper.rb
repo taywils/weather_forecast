9fa24416044083eed2e144be396563061c373c84
Demetrious Wilson <demetriouswilson@gmail.com>
2024-01-31 01:56:08 -0800

    Add view_component update spec/rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,3 +76,13 @@ end
 
 # Configure Capybara to use :cuprite driver by default
 Capybara.javascript_driver = :cuprite
+
+require 'view_component/test_helpers'
+require 'view_component/system_test_helpers'
+require 'capybara/rspec'
+
+RSpec.configure do |config|
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include ViewComponent::SystemTestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
+end
