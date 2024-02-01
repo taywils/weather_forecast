a710611b28b6889711f5b4e15bf0550d2a6bc8e8
Demetrious Wilson <demetriouswilson@gmail.com>
2024-02-01 00:23:50 -0800

    Add comments for Interactor

--- app/services/weather_organizer.rb
+++ app/services/weather_organizer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+# The WeatherOrganizer class is responsible for organizing the workflow of fetching, parsing, and building weather data.
 class WeatherOrganizer
   include Interactor::Organizer
 
+  # The organize method defines the interactors that make up this organizer.
+  # The interactors are run in the order they are listed.
+  # The `context` of each interactor is passed to the next interactor in the chain.
   organize WeatherFetcher, WeatherParser, WeatherBuilder
 end
